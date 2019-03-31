28aa05c1a479de8a1ee2fb530052e1d0c80bb949
Your Name <you@example.com>
2019-03-30 21:05:27 -0400

    Added routing pages

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
     "URI" : "mongodb://localhost/ContactList",
-    //"URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson10b",
+    "URI" : "mongodb://ishrat:ishrat123@ds060977.mlab.com:60977/comp308-assignment2",
     "secret": "SomeSecret"
 }
