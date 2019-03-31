5c17c486e5b4e20d9bad817bd5392502b0708a2e
Your Name <you@example.com>
2019-03-30 22:14:48 -0400

    updated heroku app

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "URI" : "mongodb://localhost/ContactList",
+    // "URI" : "mongodb://localhost/ContactList",
     "URI" : "mongodb://ishrat:ishrat123@ds060977.mlab.com:60977/comp308-assignment2",
     "secret": "SomeSecret"
 }
